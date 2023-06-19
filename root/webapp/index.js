c1d57681f2c30396ddbe9121eefc9e97ea8bb1a9
Stanislaus Kopacz <stanislaus.kopacz@regioit.de>
2023-06-19 13:13:32 +0000

    add Appcontroller and App.view.xml

--- root/webapp/index.js
+++ root/webapp/index.js
@@ -1,14 +1,9 @@
 sap.ui.define([
-    "sap/m/Button",
-    'sap/m/MessageToast'
-], function(Button, MessageToast) {
-    'use strict';
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
 
-    new Button({
-        Text: "Ready....",
-        press: function(){
-            MessageToast.show("Hello World!");
-        }
-    }).placeAt("content");
-    
+	XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
+		oView.placeAt("content");
+	});
 });
